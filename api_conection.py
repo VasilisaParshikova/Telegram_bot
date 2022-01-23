0e92feebe6df1675a1a45005ec784768ca60ed7f
Vasilisa <vasilisazaykova@gmail.com>
2022-01-23 23:27:52 +0500

    first chat and api

--- api_conection.py
+++ api_conection.py
@@ -1,10 +1,11 @@
 import os
-
 import requests
 import json
+from dotenv import load_dotenv
+load_dotenv()
 url = "https://hotels4.p.rapidapi.com/locations/v2/search"
 
-querystring = {"query":"екатеринбург"}
+querystring = {"query": "екатеринбург"}
 
 headers = {
     'x-rapidapi-host': "hotels4.p.rapidapi.com",
@@ -14,18 +15,27 @@ headers = {
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 sity_dict = json.loads(response.text)
+print(sity_dict)
 sity_id = sity_dict['suggestions'][0]['entities'][0]['destinationId']
-
+hotel_amount = 5
 
 url = "https://hotels4.p.rapidapi.com/properties/list"
 
-querystring = {"destinationId":sity_id,"pageNumber":"1","pageSize":"5","checkIn":"2020-01-08","checkOut":"2020-01-15","adults1":"1","sortOrder":"PRICE_HIGHEST_FIRST"}
-
-headers = {
-    'x-rapidapi-host': "hotels4.p.rapidapi.com",
-    'x-rapidapi-key': "fcdef2e919mshede91a9f6719bc0p14536bjsn26e25f9238d5"
-    }
-
+querystring = {"destinationId": sity_id, "pageNumber": "1", "pageSize": str(hotel_amount), "checkIn": "2020-02-02", "checkOut": "2020-02-15", "adults1": "1", "sortOrder": "PRICE"}
 response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
+hotels_dict = json.loads(response.text)
+hotels_dict = hotels_dict['data']['body']['searchResults']['results']
+print(hotels_dict)
+print(hotels_dict[0]['guestReviews']['rating'])
+results = []
+hotel_id_in_res = 0
+for hotel_info in hotels_dict:
+    results.append(dict())
+    results[hotel_id_in_res]['name'] = hotel_info['name']
+    results[hotel_id_in_res]['starRating'] = hotel_info['starRating']
+    results[hotel_id_in_res]['address'] = hotel_info['address']['streetAddress']
+#    results[hotel_id_in_res]['guestRating'] = hotel_info['guestReviews']['rating']
+    results[hotel_id_in_res]['price'] = hotel_info['ratePlan']['price']['current']
+    hotel_id_in_res += 1
+for hotel in results:
+    print(hotel)
--- main2.py
+++ main2.py
@@ -1,16 +1,24 @@
 import os
-
+import requests
+import json
 import telebot
 from dotenv import load_dotenv
 from telebot import types
 
 load_dotenv()
-print(os.getenv("TELEGRAM_BOT_TOKEN", "Put here your token"))
+headers = {
+    'x-rapidapi-host': "hotels4.p.rapidapi.com",
+    'x-rapidapi-key': os.getenv("RAPID_API_KEY", "")
+}
+
+
 bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN", "Put here your token"))
+
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Я на связи. Для получения списка команд напишите /help')
+
 
 @bot.message_handler(commands=["help"])
 def help(m, res=False):
@@ -30,26 +38,138 @@ def help(m, res=False):
                                 "/history — вывод истории поиска отелей\n", reply_markup=markup)
 
 
+sity = ''
+sity_id = 0
+hotel_amount = 0
+foto_flag = False
+foto_amount = 0;
+
 
 @bot.message_handler(commands=["lowprice"])
-def lowprice(m, res=False):
+def lowprice(message, res=False):
     no_keyboard = types.ReplyKeyboardRemove()
-    bot.send_message(m.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+    bot.send_message(message.chat.id, "В каком городе искать отели?", reply_markup=no_keyboard)
+    bot.register_next_step_handler(message, get_sity)
+
+
+def get_sity(message):
+    global sity
+    global sity_id
+    sity = message.text
+    url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+    querystring = {"query": sity}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    sity_dict = json.loads(response.text)
+    if len(sity_dict['suggestions'][0]['entities']) == 0:
+        bot.send_message(message.from_user.id, 'Ошибка в написании названия города.'
+                                               'Попроуйте ввести название города заново.')
+        bot.register_next_step_handler(message, get_sity)
+    else:
+        sity_id = sity_dict['suggestions'][0]['entities'][0]['destinationId']
+        bot.send_message(message.from_user.id, 'Сколько отелей вывести в результате поиска? (не более 10)')
+        bot.register_next_step_handler(message, get_hotel_amount)
+
+
+def get_hotel_amount(message):
+    global hotel_amount
+    hotel_amount = int(message.text)
+    if hotel_amount > 10 or hotel_amount < 1:
+        bot.send_message(message.from_user.id, 'Указано не верное количество отелей. Можно запросить от 1 до 10')
+        bot.register_next_step_handler(message, get_hotel_amount)
+    else:
+        bot.send_message(message.from_user.id, 'Включать в результаты поиска фото отелей? (да/нет)')
+        bot.register_next_step_handler(message, get_foto_flag)
+
+
+def get_foto_flag(message):
+    global foto_amount
+    global foto_flag
+    foto_flag = message.text
+    if foto_flag.lower() == 'да':
+        bot.send_message(message.from_user.id, 'Укажите количетсво фотографий для каждого отеля (от 1 до 10)')
+        bot.register_next_step_handler(message, get_foto_amount)
+    elif foto_flag.lower() == 'нет':
+        foto_amount = 0
+        answer = result_func()
+        bot.send_message(message.from_user.id, answer)
+    else:
+        bot.send_message(message.from_user.id, 'Некорректный ввод. Напишите да или нет.')
+        bot.register_next_step_handler(message, get_foto_flag)
+
+
+def get_foto_amount(message):
+    global foto_amount
+    foto_amount = int(message.text)
+    if foto_amount > 10 or foto_amount < 1:
+        bot.send_message(message.from_user.id, 'Указано не верное количество фотографий. Можно запросить от 1 до 10')
+        bot.register_next_step_handler(message, get_foto_amount)
+    else:
+        answer = result_func()
+        bot.send_message(message.from_user.id, answer)
+
+
+def result_func():
+    global foto_amount
+    global hotel_amount
+    global sity
+    global sity_id
+
+
+    # bot.send_message(message.from_user.id, 'Необходимо найти {} отелей в городе {}.'
+    #                                       'Результаты поика отсортировать по увеличению цены.'
+    #                                       'Для каждого отеля выводить {} фотографий'.format(
+    #    hotel_amount, sity, foto_amount
+    # ))
+    headers = {
+        'x-rapidapi-host': "hotels4.p.rapidapi.com",
+        'x-rapidapi-key': os.getenv("RAPID_API_KEY", "")
+    }
+    url = "https://hotels4.p.rapidapi.com/properties/list"
+
+    querystring = {"destinationId": sity_id, "pageNumber": "1", "pageSize": str(hotel_amount), "checkIn": "2020-02-02",
+                   "checkOut": "2020-02-15", "adults1": "1", "sortOrder": "PRICE"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    hotels_dict = json.loads(response.text)
+    hotels_dict = hotels_dict['data']['body']['searchResults']['results']
+    results = []
+    hotel_id_in_res = 0
+    for hotel_info in hotels_dict:
+        results.append(dict())
+        results[hotel_id_in_res]['name'] = hotel_info['name']
+        results[hotel_id_in_res]['starRating'] = hotel_info['starRating']
+        results[hotel_id_in_res]['address'] = hotel_info['address']['streetAddress']
+        #    results[hotel_id_in_res]['guestRating'] = hotel_info['guestReviews']['rating']
+        results[hotel_id_in_res]['price'] = hotel_info['ratePlan']['price']['current']
+        hotel_id_in_res += 1
+    answer = []
+
+    for hotel in results:
+        answer.append('Отель {} {} звезды.\nАдрес:{}.\nЦена {}\n'.format(
+            hotel['name'], hotel['starRating'], hotel['address'], hotel['price']
+        ))
+    answer = ''.join(answer)
+    return answer
+
+
+
 
 @bot.message_handler(commands=["highprice"])
-def highprice(m, res=False):
+def highprice(message, res=False):
     no_keyboard = types.ReplyKeyboardRemove()
-    bot.send_message(m.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+    bot.send_message(message.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+
 
 @bot.message_handler(commands=["bestdeal"])
-def bestdeal(m, res=False):
+def bestdeal(message, res=False):
     no_keyboard = types.ReplyKeyboardRemove()
-    bot.send_message(m.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+    bot.send_message(message.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+
 
 @bot.message_handler(commands=["history"])
-def history(m, res=False):
+def history(message, res=False):
     no_keyboard = types.ReplyKeyboardRemove()
-    bot.send_message(m.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+    bot.send_message(message.chat.id, "Позже тут что-то будет", reply_markup=no_keyboard)
+
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -59,5 +179,6 @@ def handle_text(message):
                                           "Напиши /help, чтобы получить список команд")
     else:
         bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help.")
+
 
 bot.polling(none_stop=True, interval=0)
