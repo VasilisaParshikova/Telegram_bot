71fcdb9c10915892fc87868bdf9a4dcbc649c3b2
Vasilisa <vasilisazaykova@gmail.com>
2022-04-21 00:33:49 +0500

    try_1

--- api_singleton.py
+++ api_singleton.py
@@ -25,3 +25,32 @@ class ApiSgltn:
         else:
             sity_id = sity_dict['suggestions'][0]['entities'][0]['destinationId']
             return sity_id
+
+    def get_results(self, sity_id: int, hotel_amount: int, req_type: int):
+        url = "https://hotels4.p.rapidapi.com/properties/list"
+        req_types = {0: "PRICE"}
+
+        querystring = {"destinationId": sity_id , "pageNumber": "1", "pageSize": hotel_amount,
+                       "checkIn": "2020-06-02",
+                       "checkOut": "2020-06-03", "adults1": "1", "sortOrder": req_types[req_type]}
+        response = requests.request("GET", url, headers=self.__headers, params=querystring)
+        hotels_dict = json.loads(response.text)
+        hotels_dict = hotels_dict['data']['body']['searchResults']['results']
+        results = []
+        hotel_id_in_res = 0
+        for hotel_info in hotels_dict:
+            results.append(dict())
+            results[hotel_id_in_res]['name'] = hotel_info['name']
+            results[hotel_id_in_res]['starRating'] = hotel_info['starRating']
+            results[hotel_id_in_res]['address'] = hotel_info['address']['streetAddress']
+            #    results[hotel_id_in_res]['guestRating'] = hotel_info['guestReviews']['rating']
+            results[hotel_id_in_res]['price'] = hotel_info['ratePlan']['price']['current']
+            hotel_id_in_res += 1
+        answer = []
+
+        for hotel in results:
+            answer.append('Отель {} {} звезды.\nАдрес:{}.\nЦена {}\n'.format(
+                hotel['name'], hotel['starRating'], hotel['address'], hotel['price']
+            ))
+        answer = ''.join(answer)
+        return answer
--- handlers.py
+++ handlers.py
@@ -1,6 +1,9 @@
 import telebot
 from telebot import types
 from api_singleton import ApiSgltn
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from datetime import date
+
 class SearchRequest:
     def __init__(self, type: int, message: types.Message, bot: telebot.TeleBot, api_worker: ApiSgltn):
         self.__type = type
@@ -16,36 +19,43 @@ class SearchRequest:
         self.__api_worker = api_worker
         no_keyboard = types.ReplyKeyboardRemove()
         bot.send_message(message.chat.id, "В каком городе искать отели?", reply_markup=no_keyboard)
-        bot.register_next_step_handler(message, self.get_sity)
+        bot.register_next_step_handler(message, self.set_sity)
 
-    def get_sity(self, message: types.Message):
+    def set_sity(self, message: types.Message):
         self.__sity = message.text
         answer = self.__api_worker.sity_request(self.__sity)
         if answer:
             self.__sity_id = answer
-            self.__bot.send_message(message.from_user.id, 'Сколько отелей вывести в результате поиска? (не более 10)')
-            self.__bot.register_next_step_handler(message, self.get_hotel_amount)
+            self.__bot.send_message(message.from_user.id, 'Укажите дату заезда в формате гггг-мм-дд')
+            self.__bot.register_next_step_handler(message, self.set_date_check_in)
         else:
             self.__bot.send_message(message.from_user.id, 'Ошибка в написании названия города.'
                                                           'Попроуйте ввести название города заново.')
-            self.__bot.register_next_step_handler(message, self.get_sity)
+            self.__bot.register_next_step_handler(message, self.set_sity)
 
-    def get_hotel_amount(self, message: types.Message):
+    def set_date_check_in(self, message: types.Message):
+        ######################## потом нужно получение даты выезда!######
+        data = message.text
+        self.__bot.send_message(message.from_user.id, 'Сколько отелей вывести в результате поиска? (не более 10)')
+        self.__bot.register_next_step_handler(message, self.set_hotel_amount)
+
+
+    def set_hotel_amount(self, message: types.Message):
         self.__hotel_amount = int(message.text)
         if self.__hotel_amount > 10 or self.__hotel_amount < 1:
             self.__bot.send_message(message.from_user.id, 'Указано не верное количество отелей. '
                                                           'Можно запросить от 1 до 10')
-            self.__bot.register_next_step_handler(message, self.__get_hotel_amount)
+            self.__bot.register_next_step_handler(message, self.set_hotel_amount)
         else:
             self.__bot.send_message(message.from_user.id, 'Включать в результаты поиска фото отелей? '
                                                           '(да/нет)')
-            self.__bot.register_next_step_handler(message, self.get_foto_flag)
+            self.__bot.register_next_step_handler(message, self.set_foto_flag)
 
-    def get_foto_flag(self, message: types.Message):
+    def set_foto_flag(self, message: types.Message):
         self.__foto_flag = message.text
         if self.__foto_flag.lower() == 'да':
             self.__bot.send_message(message.from_user.id, 'Укажите количетсво фотографий для каждого отеля (от 1 до 10)')
-            self.__bot.register_next_step_handler(message, self.__get_foto_amount)
+            self.__bot.register_next_step_handler(message, self.set_foto_amount)
         elif self.__foto_flag.lower() == 'нет':
             ######################################################
             ###### корректировать!! ##
@@ -55,16 +65,22 @@ class SearchRequest:
             self.__bot.send_message(message.from_user.id, answer)
         else:
             self.__bot.send_message(message.from_user.id, 'Некорректный ввод. Напишите да или нет.')
-            self.__bot.register_next_step_handler(message, self.__get_foto_flag)
+            self.__bot.register_next_step_handler(message, self.set_foto_flag)
 
-    def __get_foto_amount(self, message: types.Message):
+    def set_foto_amount(self, message: types.Message):
         self.__foto_amount = int(message.text)
         if self.__foto_amount > 10 or self.__foto_amount < 1:
             self.__bot.send_message(message.from_user.id,
                                 'Указано не верное количество фотографий. Можно запросить от 1 до 10')
-            self.__bot.register_next_step_handler(message, self.__get_foto_amount)
+            self.__bot.register_next_step_handler(message, self.set_foto_amount)
         else:
             answer = self.result_func()
             self.__bot.send_message(message.from_user.id, answer)
 
+    def get_req_info(self):
+        return [self.__sity_id, self.__hotel_amount, self.__type]
+
     def result_func(self):
+        data = self.get_req_info()
+        return self.__api_worker.get_results(data[0], data[1], data[2])
+
